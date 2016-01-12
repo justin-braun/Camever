63ab4d5cb607a6671bcca6f1aa806306d9354dd3
Justin Braun <justin@braunweb.com>
2016-01-12 11:38:10 -0600

    added setup package and added single instance method.

--- WebCamCapture/Program.cs
+++ WebCamCapture/Program.cs
@@ -1,22 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace SpryCoder.WebcamCaptureTool
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "<53FC12C6-7064-46B6-916B-FB97CC89137A>");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+
+            // Blocks the current thread until the current instance receives a signal,
+            // using a TimeSpan to specify the time interval and specifying whether to exit the synchronization domain before the wait.
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+
+                // Releases the Mutex once.
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("There is already an instance of this application running.", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
