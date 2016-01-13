259ff455d1a1b1f5a2ba269a93d5e7f8e0870211
Justin Braun <justin@braunweb.com>
2016-01-13 09:49:28 -0600

    Added BetaHelper class.

---
+++ WebCamCapture/BetaHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpryCoder.Camever
+{
+    public static class BetaHelper
+    {
+        private static readonly DateTime BetaExpireDate = DateTime.Parse("03/15/2016");
+
+        public static bool BetaExpired()
+        {
+            return (DateTime.Now > BetaExpireDate) ? true : false;
+        }
+    }
+}
